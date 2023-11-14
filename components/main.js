e12565ab4079c976f6eea38b2de084b68c6181e1
Bruno Bossois Hohmann <gnobr1@gmail.com>
2023-11-14 20:02:42 -0300

    Corrige o JS e suas funções

--- components/main.js
+++ components/main.js
@@ -1,45 +1,45 @@
 function checkTime() {
-    // Otimizado por IA
-    var diasSemana = ["domingo", "segunda", "terca", "quarta", "quinta", "sexta", "sabado"];
-    var day = new Date().getDay();
-    var hour = new Date().getHours();
+    //Reescrita do código original feita por IA, com intuito de facilitar a manutenção, leitura e otimização do mesmo
+    //** Adicionado conteúdo por mim no último else, corrigindo alguns erros e adicionando funcionalidades
 
-    var diaAtual = diasSemana[day];
+    const diasSemana = ["domingo", "segunda", "terca", "quarta", "quinta", "sexta", "sabado"];
+    const day = new Date().getDay();
+    const hour = new Date().getHours();
+    const diaAtual = diasSemana[day];
 
-    if (hour < 20) {
-        document.querySelector(`#${diaAtual}`).firstElementChild.classList.add("ativo");
-        document.getElementById(diaAtual).style.backgroundColor = 'rgb(194, 194, 194)';
-    } else {
-        var proximoDia = diasSemana[(day + 1) % 7];
-        document.querySelector(`#${proximoDia}`).firstElementChild.classList.add("ativo");
-        document.querySelector(`#jantar${diaAtual.charAt(0).toUpperCase() + diaAtual.slice(1)}`).classList.remove("ativo");
-
-        if (document.querySelector(`#${diaAtual}`).firstElementChild.classList != null) {
-            document.getElementById(diaAtual).style.backgroundColor = 'inherit';
-            document.getElementById(proximoDia).style.backgroundColor = 'rgb(194, 194, 194)';
+    const setAtivo = (elemento, sufixo) => {
+        document.querySelector(`#${elemento}`).firstElementChild.classList.add("ativo");
+        document.getElementById(elemento).style.backgroundColor = 'rgb(194, 194, 194)';
+        if (hour >= 8 && hour < 10) {
+            document.querySelector("#cafe").classList.add("ativo");
+            document.querySelector(`#cafe${sufixo}`).classList.add("ativo");
+        } else if (hour >= 10 && hour < 14) {
+            document.querySelector("#almoco").classList.add("ativo");
+            document.querySelector(`#almoco${sufixo}`).classList.add("ativo");
+        } else if (hour >= 14 && hour < 17) {
+            document.querySelector("#lanche").classList.add("ativo");
+            document.querySelector(`#lanche${sufixo}`).classList.add("ativo");
+        } else if (hour >= 17 && hour < 20) {
+            document.querySelector("#jantar").classList.add("ativo");
+            document.querySelector(`#jantar${sufixo}`).classList.add("ativo");
         } else {
-            document.getElementById(diaAtual).firstElementChild.classList.remove("ativo");
+            const proximoDia = diasSemana[(day + 1) % 7];
+            document.querySelector(`#cafe`).classList.add("ativo");
+            document.querySelector(`#cafe${proximoDia.charAt(0).toUpperCase() + proximoDia.slice(1)}`).classList.add("ativo");
+
+            document.querySelector(`#titulo${proximoDia.charAt(0).toUpperCase() + proximoDia.slice(1)}`).classList.add("ativo");
+            document.querySelector(`#titulo${sufixo}`).classList.remove("ativo");
+
+            document.querySelector(`#jantar`).classList.remove("ativo");
+            document.querySelector(`#jantar${sufixo}`).classList.remove("ativo");
+
+            document.getElementById(elemento).style.backgroundColor = 'inherit';
+            document.getElementById(proximoDia).style.backgroundColor = 'rgb(194, 194,194)'
         }
-    }
+    };
 
+    setAtivo(diaAtual, diaAtual.charAt(0).toUpperCase() + diaAtual.slice(1));
 
-    if (hour >= 8 && hour < 10) {
-        document.querySelector("#cafe").classList.add("ativo");
-        document.querySelector(`#cafe${diaAtual.charAt(0).toUpperCase() + diaAtual.slice(1)}`).classList.add("ativo");
-    } else if (hour >= 10 && hour < 14) {
-        document.querySelector("#almoco").classList.add("ativo");
-        document.querySelector(`#almoco${diaAtual.charAt(0).toUpperCase() + diaAtual.slice(1)}`).classList.add("ativo");
-    } else if (hour >= 14 && hour < 17) {
-        document.querySelector("#lanche").classList.add("ativo");
-        document.querySelector(`#lanche${diaAtual.charAt(0).toUpperCase() + diaAtual.slice(1)}`).classList.add("ativo");
-    } else if (hour >= 17 && hour < 20) {
-        document.querySelector("#jantar").classList.add("ativo");
-        document.querySelector(`#jantar${diaAtual.charAt(0).toUpperCase() + diaAtual.slice(1)}`).classList.add("ativo");
-    } else {
-        var proximoDia = diasSemana[(day + 1) % 7];
-        document.querySelector(`#jantar${diaAtual.charAt(0).toUpperCase() + diaAtual.slice(1)}`).classList.remove("ativo");
-        document.querySelector(`#cafe${proximoDia.charAt(0).toUpperCase() + proximoDia.slice(1)}`).classList.add("ativo");
-    }
     /*  Escrito por mim
         let day = new Date().getDay();
         let hour = new Date().getHours();
@@ -171,11 +171,10 @@ function checkTime() {
                 document.querySelector("#jantarDomingo").classList.add("ativo");
             }
         } */
-};
-
+}
 
 for (var i = 0; i <= 0; i++) {
     checkTime();
 }
 
-setInterval(checkTime, 10 * 1000);
+setInterval(checkTime, 90 * 1000);
