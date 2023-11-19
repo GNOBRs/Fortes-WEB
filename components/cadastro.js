4a0aebf753c94a12175f80d6290f59dc7f8c8596
Bruno Bossois Hohmann <gnobr1@gmail.com>
2023-11-19 13:21:29 -0300

    Finaliza a criação da página de cadastro

--- components/cadastro.js
+++ components/cadastro.js
@@ -1,17 +1,22 @@
-document.querySelector(`#salvar`).addEventListener("click", function (event) {
-    window.alert("Cadastro feito com sucesso");
-
-    const form = document.querySelector("form");
-
-    form.reset();
-
-    /*     const valorInput = document.querySelectorAll("form");
-    
-        valorInput.forEach(
-            function (valor) {
-                valor.reset();
-            }
-        ); */
-
+document.querySelector("#salvar").addEventListener("click", function (event) {
     event.preventDefault();
-})
+
+    const inputs = document.querySelectorAll("form input");
+
+    let isFormValid = true;
+
+    inputs.forEach(function (input) {
+        if (input.value.trim() === "") {
+            isFormValid = false;
+        }
+    });
+
+    if (isFormValid) {
+        window.alert("Cadastro feito com sucesso");
+
+        const form = document.querySelector("form");
+        form.reset();
+    } else {
+        window.alert("Por favor, preencha todos os campos antes de salvar.");
+    }
+});
